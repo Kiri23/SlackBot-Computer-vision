9e648829971b7ea7c3c9cd440adf534d413a3666
Christian Nogueras <Christiannogueras@MBPdeChristian.lan>
2017-05-07 11:32:26 -0400

    fix typo

--- Bot.js
+++ Bot.js
@@ -596,6 +596,6 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.listen(process.env.port || 5000, function () {
+app.listen(process.env.PORT || 5000, function () {
   console.log('Example app listening on port 5000!')
 })
